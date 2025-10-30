6e7b0cfb080929d4c5bcae9fce1a394b21c2769d
Isaque Lucas <isaquelucasvelozope@gmail.com>
2025-10-30 09:41:04 -0300

    Tentativa de orientação para as funções utilitarias..

--- HOROGO/Modulos/Autenticacao/tentativa.py
+++ HOROGO/Modulos/Autenticacao/tentativa.py
@@ -1,3 +1,8 @@
+import getpass
+import time
+
+from HOROGO.Modulos.utilitarios import limpar_terminal, salvar_conta, carregar_conta
+
 class Usuario:
     """A classe de Usuario na memoria do Python. Apenas representa um unico usuario, e não todos."""
     # Os comentarios aqui dentro depois vão ser perfurmados, mas por enquanto NÃO apague ate a gente entender como que funciona toda a obra.
@@ -26,12 +31,34 @@ class Usuario:
 
     # Faz com que esse metodo seja algo para a classe, não para a instancia de uma classe. Ou basicamente vira uma função global para a classe.
     @classmethod
-    def from_dict(cls, data):
+    def from_dict(cls, dados):
         # cls nesse caso aqui e a classe de objeto de usuario. então ele ta retornand o dicinario como uma classe de Usuario.
         return cls(
-            data['usuario'],
-            data['senha'],
-            data['instituicao'],
-            data['periodo_atual']
+            dados['usuario'],
+            dados['senha'],
+            dados['instituicao'],
+            dados['periodo_atual']
         )
         
+class Autenticacao:
+    """Sistema de login e cadastro."""
+    def __init__(self):
+        self.usuarios = self._carregar_usuarios()
+
+    def _carregar_usuarios(self):
+
+        conta = carregar_conta()
+        usuario_objeto = {}
+        for usuario, dados in conta.items():
+            usuario_objeto[usuario] = Usuario.from_dict(dados)
+        return usuario_objeto
+
+    def _salvar_usuario(self):
+        conta = {}
+        for usuario, usuario_objeto in self.usuarios.items():
+            conta[usuario] = usuario_objeto.dicionario()
+        salvar_conta(conta)
+    
+    
+
+
